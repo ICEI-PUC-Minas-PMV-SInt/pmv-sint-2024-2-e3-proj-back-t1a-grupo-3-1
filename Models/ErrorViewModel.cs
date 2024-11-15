08eda2d2aad67fbdae802cffc7b4e5b87e31af07
Glauber Lanna <glauberlanna@hotmail.com>
2024-11-15 10:33:18 -0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Projeto_1._0_eixo3_2024.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
