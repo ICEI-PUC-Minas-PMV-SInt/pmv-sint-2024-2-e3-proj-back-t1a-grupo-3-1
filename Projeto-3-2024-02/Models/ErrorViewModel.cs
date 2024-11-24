2e66504efe1391cd2a8d46176b3f88e77d946801
glauberlanna <61885020+glauberlanna@users.noreply.github.com>
2024-11-24 13:51:29 -0300

    Delete Projeto-3-2024-02 directory
    ENVIO APAGADO, PARA NOVO COMMIT COM O PROJETO ATUALIZADO

--- Projeto-3-2024-02/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Projeto_3_2024_02.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
